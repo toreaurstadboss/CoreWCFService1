38a2d1c58ba97a71d2ba997ada7ef8c98ffccda3
Tore Aurstad <tore.aurstad@gmail.com>
2023-11-22 18:49:51 +0100

    Init commit

---
+++ BasicHttpClientHandler.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Runtime.CompilerServices;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Xml.Linq;
+
+
+public static class HttpRequestExtensions
+{
+
+    public static string? GetAuthenticationHeaderFromSoapEnvelope(this HttpRequest request)
+    {
+        // read out body (wait for all bytes)
+        using (var streamCopy = new MemoryStream())
+        {
+            request.Body.CopyTo(streamCopy);
+            streamCopy.Position = 0; // rewind
+
+            string body = new StreamReader(streamCopy).ReadToEnd();
+
+            string authTicketFromHeader = null;
+
+            if (body?.Contains(@"http://schemas.xmlsoap.org/soap/envelope/") == true)
+            {
+                XNamespace ns = "http://schemas.xmlsoap.org/soap/envelope/";
+                var soapEnvelope = XDocument.Parse(body);
+                var headers = soapEnvelope.Descendants(ns + "Header").ToList();
+
+                foreach (var header in headers)
+                {
+                    var authorizationElement = header.Element("Authorization");
+                    if (!string.IsNullOrWhiteSpace(authorizationElement?.Value))
+                    {
+                        authTicketFromHeader = authorizationElement.Value;
+                        break;
+                    }
+                }
+            }
+
+            streamCopy.Position = 0; // rewind again
+            //request.Body = streamCopy; // put back in place for downstream handlers
+
+            //request.Headers.Add("Authorization", authTicketFromHeader);
+
+            return authTicketFromHeader;
+        }
+
+    }
+
+}
+
+public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+{
+
+    private readonly IUserRepository _userRepository;
+    public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder,
+        ISystemClock clock, IUserRepository userRepository) :
+       base(options, logger, encoder, clock)
+    {
+        _userRepository = userRepository;
+    }
+
+    protected async override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var identity = new GenericIdentity("letmein");
+        var claimsPrincipal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(claimsPrincipal, Scheme.Name);
+        return await Task.FromResult(AuthenticateResult.Success(ticket));
+
+
+        //string? authTicketFromSoapEnvelope = Request.GetAuthenticationHeaderFromSoapEnvelope();
+
+        //if (authTicketFromSoapEnvelope != null && authTicketFromSoapEnvelope.StartsWith("basic", StringComparison.OrdinalIgnoreCase))
+        //{
+        //    var token = authTicketFromSoapEnvelope.Substring("Basic ".Length).Trim();
+        //    var credentialsAsEncodedString = Encoding.UTF8.GetString(Convert.FromBase64String(token));
+        //    var credentials = credentialsAsEncodedString.Split(':');
+        //    if (await _userRepository.Authenticate(credentials[0], credentials[1]))
+        //    {
+        //        //var claims = new[] { new Claim("name", credentials[0]), new Claim(ClaimTypes.Role, "Admin") };
+        //        var identity = new GenericIdentity(credentials[0]);
+        //        var claimsPrincipal = new ClaimsPrincipal(identity);
+        //        var ticket = new AuthenticationTicket(claimsPrincipal, Scheme.Name);
+        //        return await Task.FromResult(AuthenticateResult.Success(ticket));
+        //    }
+    }
+
+
+        //Response.StatusCode = 401;
+        //Response.Headers.Add("WWW-Authenticate", "Basic realm=\"thoushaltnotpass.com\"");
+        //return await Task.FromResult(AuthenticateResult.Fail("Invalid Authorization Header"));
+    
+
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using CoreWCF.Security;
+using Microsoft.AspNetCore.Authentication;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder();
+
+builder.WebHost.ConfigureKestrel(opt => opt.AllowSynchronousIO = true);
+
+builder.Services.AddServiceModelServices();
+builder.Services.AddServiceModelMetadata();
+builder.Services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
+
+builder.Services.AddSingleton<IUserRepository, UserRepository>();
+
+if(ServicePointManager.SecurityProtocol.HasFlag(SecurityProtocolType.Tls12) == false)
+{
+    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+}
+
+builder.Services.AddAuthentication("Basic").
+            AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>
+            ("Basic", null);
+
+builder.Services.AddScoped<Service>();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+//app.UseAuthorization();
+
+app.Use(async (context, next) =>
+{
+    // Only check for basic auth when path is for the TransportWithMessageCredential endpoint only
+    if (context.Request.Path.StartsWithSegments("/Service.svc"))
+    {
+        // Check if currently authenticated
+        var authResult = await context.AuthenticateAsync("Basic");
+        if (authResult.None)
+        {
+            // If the client hasn't authenticated, send a challenge to the client and complete request
+            await context.ChallengeAsync("Basic");
+            return;
+        }
+    }
+    // Call the next delegate/middleware in the pipeline.
+    // Either the request was authenticated of it's for a path which doesn't require basic auth
+    await next(context);
+});
+
+app.UseServiceModel(serviceBuilder =>
+{
+    var basicHttpBinding = new BasicHttpBinding(BasicHttpSecurityMode.Transport);
+    basicHttpBinding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Basic;
+    serviceBuilder.AddService<Service>();
+    serviceBuilder.AddServiceEndpoint<Service, IService>(basicHttpBinding, "/Service.svc");
+
+    var security = SecurityBindingElement.CreateUserNameOverTransportBindingElement();
+    security.SecurityHeaderLayout = SecurityHeaderLayout.Lax;
+
+
+    var serviceMetadataBehavior = app.Services.GetRequiredService<ServiceMetadataBehavior>();
+    serviceMetadataBehavior.HttpsGetEnabled = true;
+});
+
+app.Run();
