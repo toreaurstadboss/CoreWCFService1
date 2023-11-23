36695350202d01d66c39cda502b5735eaebbaf0a
Tore Aurstad <tore.aurstad@gmail.com>
2023-11-23 16:22:41 +0100

    Splitting a file into two

--- IUserRepository.cs
+++ IUserRepository.cs
@@ -1,22 +1,9 @@
 ﻿namespace CoreWCFService1
 {
+
     public interface IUserRepository
     {
-
         public Task<bool> Authenticate(string username, string password);
-    }
-
-    public class UserRepository : IUserRepository
-    {
-        public Task<bool> Authenticate(string username, string password)
-        {
-            //TODO: some dummie auth mechanism used here, make something more realistic such as DB user repo lookup or similar
-            if (username == "someuser" && password == "somepassw0rd")
-            {
-                return Task.FromResult(true);
-            }
-            return Task.FromResult(false);
-        }
     }
 
 }
